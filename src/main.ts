295b875675c02e278a16b3764c4865bb0dae4759
jefaokpta <jefaokpta@hotmail.com>
2025-02-25 12:15:27 -0300

    Update default port to 3000

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 
   await app.startAllMicroservices();
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
