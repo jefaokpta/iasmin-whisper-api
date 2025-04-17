ecb7b3e756304dc57cafc822904de13485eb8197
jefaokpta <jefaokpta@hotmail.com>
2025-04-17 11:09:17 -0300

    IA: Remover async/await de consume no Kafka Controller

--- src/kafka/kafka.controller.ts
+++ src/kafka/kafka.controller.ts
@@ -2,14 +2,13 @@ import { Controller } from '@nestjs/common';
 import { RecognitionService } from '../recognition/recognition.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Cdr } from '../model/cdr';
+
 @Controller('kafka')
 export class KafkaController {
   constructor(private readonly recognitionService: RecognitionService) {}
 
-
   @MessagePattern('transcriptions')
-  async consume(@Payload() cdr: Cdr) {
-      await this.recognitionService.start(cdr);
+  consume(@Payload() cdr: Cdr) {
+    this.recognitionService.start(cdr);
   }
-    
 }
